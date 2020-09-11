551269336c9b3b17fcd85dccdc47e06d7650177a
夏云浩 <xyh19981224@163.com>
2020-09-11 15:26:38 +0800

    2022

---
+++ DomeOver/Cell/aaaCell/aaaTVCell.swift
@@ -0,0 +1,25 @@
+//
+//  aaaTVCell.swift
+//  DomeOver
+//
+//  Created by 寇凤伟 on 2020/9/11.
+//  Copyright © 2020 RX. All rights reserved.
+//
+
+import UIKit
+
+class aaaTVCell: UITableViewCell {
+
+    @IBOutlet weak var BiaoTi: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Pods/Target Support Files/Pods-DomeOver/Pods-DomeOver-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DomeOver : NSObject
+@end
+@implementation PodsDummy_Pods_DomeOver
+@end
